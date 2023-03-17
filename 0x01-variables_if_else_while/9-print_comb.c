c1545d8bca41badf091bbb477f6953fa978438b3
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-17 07:40:25 -0700

    Print nums separated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0 if the code executed without errors
+ */
+int main(void)
+{
+	int c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
