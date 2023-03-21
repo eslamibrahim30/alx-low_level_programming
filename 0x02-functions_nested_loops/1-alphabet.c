63c8eb0680c22616795c42d1d33c4d1e2490f2b5
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-21 16:16:28 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - print chars from a to z
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
