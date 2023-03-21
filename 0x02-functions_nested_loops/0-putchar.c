fa4040c874722cfc9a94a4135a9bd10e6f496397
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-21 07:44:04 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0 if the code executed without errors
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
