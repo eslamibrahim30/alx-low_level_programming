dccf97a158036e5798d6b9bd7e21b3d0cf297422
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-21 13:32:36 -0700

    Print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,9 +15,9 @@ int _putchar(char c)
 }
 void print_putchar()
 {
-	char word[] = "_putchar";
-	int exit_code;
-	for(int i = 0; i < strlen(word); i++)
+	char word[] = "_putchar\n";
+	int exit_code, size = 9;
+	for(int i = 0; i < size; i++)
 	{
 		exit_code = _putchar(word[i]);
 	}
