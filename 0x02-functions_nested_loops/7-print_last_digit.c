a3f91b2b16afd33cd8fa11a6e91345bf61adcfee
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-21 18:09:13 -0700

    Get last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,7 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n = -1 * n;
-	}
-	_putchar('0' + n % 10);
-	return (n % 10);
+	n = n < 0 ? -1 * (n % 10) : n % 10;
+	_putchar('0' + n);
+	return (n);
 }
