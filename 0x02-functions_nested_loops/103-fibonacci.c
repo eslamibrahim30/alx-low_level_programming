45f9bd1824fa2c4a9f8f117dddb1fda6a0796eac
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-21 19:41:31 -0700

    Even fib nums

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int a;
+	unsigned long int b;
+	unsigned long int temp;
+	int i;
+
+	a = 1;
+	b = 2;
+	for (; a <= 4000000;)
+	{
+		if (b >= 4000000 && a % 2 == 0)
+		{
+			printf("%lu\n", a);
+		}
+		else if (a % 2 == 0)
+		{
+			printf("%lu, ", a);
+		}
+		temp = a;
+		a = b;
+		b = temp + b;
+	}
+	return (0);
+}
