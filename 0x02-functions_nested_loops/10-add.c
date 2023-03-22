3c509c5e55443bdac0dce85adfe1929b3e4d90fb
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-21 18:44:00 -0700

    Add two nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * add - Calculate the sum of two given numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the sum of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
