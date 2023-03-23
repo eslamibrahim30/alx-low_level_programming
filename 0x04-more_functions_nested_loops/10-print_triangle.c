2f4fe16a593e26901e789e7dda2bdc51935debc6
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-23 09:08:31 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Draws a triangle using number of #
+ * @size: the length of the triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		for (: j < size; j++)
+		{
+			_putchar('#');
+		}
+		if (i != size - 1)
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
