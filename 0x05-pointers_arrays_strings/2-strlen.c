667ef3abad48e43129268494094e1abae68bc109
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-27 00:14:15 -0700

    Get string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,17 @@
 
 /**
  * _strlen - Get the length of the given string
- * @s: the given string
+ * @s: the given string address
  */
 int _strlen(char *s)
 {
-	int len = 0;
+	int len;
+
+	len = 0;
 	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-	return len;
+	return (len);
 }
