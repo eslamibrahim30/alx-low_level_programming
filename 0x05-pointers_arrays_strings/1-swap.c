7e912cca5bd48b089a0e1d6c5da172a74f099c1a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-27 00:07:53 -0700

    Swap two variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two variables
+ * @a: the first variable address
+ * @b: the second variable address
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *b;
+	*b = temp;
+	*a = *b;
+}
