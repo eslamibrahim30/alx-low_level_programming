a65e23b5ebb5f119669c82e5e33af600088f8c45
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-27 18:23:17 -0700

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy source string to a destination string
+ * @dest: the destination string
+ * @src: the source string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int start;
+	int end;
+
+	end = 0;
+	while (*(src + end) != '\0')
+	{
+		end++;
+	}
+	start = 0;
+	while (1)
+	{
+		*(dest + start) = *(src + start);
+		if (*(src + start) == '\0')
+		{
+			break;
+		}
+		start++;
+	}
+	return dest;
+}
