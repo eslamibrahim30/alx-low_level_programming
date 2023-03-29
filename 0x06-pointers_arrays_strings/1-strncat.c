f4d925e3a91e2a11282e3b47ee148af8067f2803
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-29 02:46:16 -0700

    Concatenate n bytes with a string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenate n bytes from source string to a destination string
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of bytes taken from source string
+ *
+ * Return: the destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int idx_dest;
+	int idx_src;
+
+	idx_dest = 0;
+	while (dest[idx_dest] != '\0')
+	{
+		idx_dest++;
+	}
+	idx_src = 0;
+	while (src[idx_src] != '\0' && idx_src < n)
+	{
+		dest[idx_dest] = src[idx_src];
+		idx_dest++;
+		idx_src++;
+	}
+	if (idx_src < n)
+	{
+		dest[idx_dest] = '\0';
+	}
+	return (dest);
+}
