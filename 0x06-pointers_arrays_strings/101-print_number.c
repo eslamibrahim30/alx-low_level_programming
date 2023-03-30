f417506d498877b97b698ed499931c75247c1256
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-03-29 19:29:11 -0700

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_number - Print a given number
+ * @n: the given number
+ */
+void print_number(int n)
+{
+	unsigned int tmp_n;
+	int len;
+	int tmp_len;
+
+	len = 1;
+	tmp_n = n < 0 ? -1 * (n + 1) : n;
+	tmp_n += n < 0 ? 1 : 0;
+	tmp_n /= 10;
+	while (tmp_n > 0)
+	{
+		tmp_n /= 10;
+		len++;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	len--;
+	while (1)
+	{
+		if (n < 0)
+		{
+			tmp_n = -1 * (n + 1);
+			tmp_n += 1;
+		}
+		else
+		{
+			tmp_n = n;
+		}
+		tmp_len = len;
+		while (tmp_len > 0)
+		{
+			tmp_n /= 10;
+			tmp_len--;
+		}
+		tmp_n %= 10;
+		_putchar('0' + tmp_n);
+		if (len <= 0)
+		{
+			break;
+		}
+		len--;
+	}
+}
