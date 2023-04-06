8a93a506ed10c99726bdc360b10d9c736e61f76d
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-04-06 04:11:42 -0700

    Compare strings using wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,7 +20,8 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s2 == '*')
 	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1 + 1, s2 + 1)
+		|| wildcmp(s1, s2 + 1));
 	}
 	else
 	{
