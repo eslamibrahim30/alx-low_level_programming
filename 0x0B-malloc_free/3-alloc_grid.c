aa8f1d9327aba10e8a717babee3798c09d043ec6
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-04-11 19:02:43 -0700

    Allocate grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocates 2 dimensional array of integers
+ * @width: the width of the array
+ * @height: the height of the array
+ *
+ * Return: the address of the allocated array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid = NULL;
+	int i;
+	int j;
+
+	if (width <= 0 && height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		grid = (int**)realloc(grid, height * sizeof(int*));
+		for (i = 0; i < height; i++)
+		{
+			grid[i] = (int*)(realloc(grid[i], width * sizeof(int)));
+			for (j = 0; j < width; j++)
+			{
+				grid[i][j] = 0;
+			}
+		}
+		return (grid);
+	}
+}
