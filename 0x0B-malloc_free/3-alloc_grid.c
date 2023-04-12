2ca491cc23bded89d8ce7cc0b02ea3ece89d1fc7
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-04-11 19:05:50 -0700

    Allocate grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,9 +21,19 @@ int **alloc_grid(int width, int height)
 	else
 	{
 		grid = (int **)realloc(grid, height * sizeof(int *));
+		if (grid == NULL)
+		{
+			free(grid);
+			return (NULL);
+		}
 		for (i = 0; i < height; i++)
 		{
 			grid[i] = (int *)(realloc(grid[i], width * sizeof(int)));
+			if (grid[i] == NULL)
+			{
+				free(grid);
+				return (NULL);
+			}
 			for (j = 0; j < width; j++)
 			{
 				grid[i][j] = 0;
