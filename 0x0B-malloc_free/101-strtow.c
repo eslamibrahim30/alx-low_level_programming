ad93a6d3d000dbf86c83537d4216855d84660d0a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-04-12 08:23:34 -0700

    String to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,88 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * countWords - Counts words in a string
+ * @str: the given string
+ *
+ * Return: the number of words in a string
+ */
+int countWords(char *str)
+{
+	int wc;
+	int i;
+	int isword;
+
+	wc = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		isword = 0;
+		while (!isspace(str[i]) && str[i] != '\0')
+		{
+			isword = 1;
+			i++;
+		}
+		if (isword)
+		{
+			wc++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (wc);
+}
+
+/**
+ * strtow - Splits a string into words
+ * @str: the given string
+ *
+ * Return: a pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int wc;
+	int cw;
+	int i;
+	int j;
+	int idx;
+
+	if (str == NULL || strlen(str) == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		wc = countWords(str);
+		words = (char **)malloc((wc + 1) * sizeof(char *));
+		cw = 0;
+		for (i = 0; i < (int)(strlen(str)); i++)
+		{
+			for (j = 0; !isspace(str[i + j]) && str[i + j] != '\0';)
+			{
+				j++;
+			}
+			if (j > 0)
+			{
+				words[cw] = (char *)malloc((j + 1) * sizeof(char));
+				idx = 0;
+				while (j > 0)
+				{
+					words[cw][idx] = str[i];
+					i++;
+					idx++;
+					j--;
+				}
+				words[cw][idx] = '\0';
+				cw++;
+			}
+		}
+		words[cw] = NULL;
+		return (words);
+	}
+}
