8f1c1b50edec06747823498f5a1f19c2ee049d4a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-04-14 17:19:59 -0700

    Multiply two large nums

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * zero_array - Initializes an array with zeros
+ * @arr: the given array
+ * @size: the size of the array
+ */
+void zero_array(int *arr, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = 0;
+	}
+}
+
+/**
+ * mul_one - Multplies big number stored in array by one digit integer
+ * @num1: the big number
+ * @size: the number of digits in the big number
+ * @n: the one digit integer
+ *
+ * Return: the address of the result
+ */
+void mul_one(int *num1, unsigned int size, int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		num1[i] *= n;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		num1[i + 1] += num1[i] / 10;
+		num1[i] %= 10;
+	}
+}
+
+/**
+ * print_large - Print large num stored in an array and ignore leading zeros
+ * @num: the large num
+ * @size: the size of the array
+ */
+void print_large(int *num, unsigned int size)
+{
+	unsigned int i;
+	int leading_zeros;
+
+	leading_zeros = 1;
+	for (i = 0; i < size; i++)
+	{
+		if (leading_zeros && num[i] == 0)
+		{
+			continue;
+		}
+		else
+		{
+			leading_zeros = 0;
+			printf("%d", *(num + i));
+		}
+	}
+	printf("\n");
+}
+
+/**
+ * reverse_nums - Reverses the order of nums in an array
+ * @arr: the given array
+ * @size: the array size
+ */
+void reverse_nums(int *arr, unsigned int size)
+{
+	unsigned int start;
+	unsigned int end;
+	int temp;
+
+	start = 0;
+	end = size - 1;
+	while (start < end)
+	{
+		temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	}
+}
+
+/**
+ * assign_large - Assigns large number to an array of integers
+ * @arr: the target array
+ * @str: the given number
+ */
+void assign_large(int *arr, char *str)
+{
+	unsigned int i;
+	unsigned int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			arr[i] = str[i] - '0';
+		}
+		else
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+}
+
+
+/**
+ * main - check the code for
+ * @argc: the number of command line arguments
+ * @argv: the command line arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int *num1;
+	int *num2;
+	int *ans;
+	unsigned int len_ans;
+	unsigned int i;
+	unsigned int j;
+	unsigned int rem;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	len_ans = (int)strlen(argv[1]) + (int)strlen(argv[2]) + 5;
+	num1 = malloc(len_ans * sizeof(int));
+	num2 = malloc(len_ans * sizeof(int));
+	ans = malloc(len_ans * sizeof(int));
+	zero_array(num2, len_ans);
+	zero_array(ans, len_ans);
+	assign_large(num2, argv[2]);
+	reverse_nums(num2, (unsigned int)(strlen(argv[2])));
+	for (i = 0; i < len_ans; i++)
+	{
+		zero_array(num1, len_ans);
+		assign_large(num1, argv[1]);
+		reverse_nums(num1, (unsigned int)(strlen(argv[1])));
+		mul_one(num1, len_ans, num2[i]);
+		rem = 0;
+		for (j = i; j < len_ans; j++)
+		{
+			ans[j] = ans[j] + num1[j - i] + rem;
+			rem = ans[j] / 10;
+			ans[j] %= 10;
+		}
+	}
+	reverse_nums(ans, len_ans);
+	print_large(ans, len_ans);
+	free(num1);
+	free(num2);
+	free(ans);
+	return (0);
+}
