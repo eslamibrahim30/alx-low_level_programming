bf9efb1d0f067b13eb72c613ab323257e40f4771
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-04-17 08:42:26 -0700

    Allocate memory for new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
