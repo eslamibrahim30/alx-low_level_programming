da1886272866965971bdb04df9fb0e2514cc1390
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-09 11:05:03 -0700

    Print program name using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char** argv)
+{
+
+	if (argc >= 1)
+	{
+	
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
