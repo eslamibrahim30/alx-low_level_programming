e966a1eddfb76719ba641c8ea4517b2511698546
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-09 12:25:16 -0700

    Add two numbers using argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of arguments of the main function
+ * @argv: the actual arguments of the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	char *j;
+	int sum;
+
+	sum = 0;
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = argv[i]; *j != '\0'; j++)
+			{
+				if (!isdigit(*j))
+				{
+					printf("Error\n");
+					return (0);
+				}
+			}
+		}
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
