437990d91c1dc631d15a873ee3ef9679ccbe20ec
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-09 11:08:11 -0700

    Print program name using argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,15 @@
 
 /**
  * main - Entry point
+ * @argc: the number of arguments of the main function
+ * @argv: the actual arguments of the main function
  *
  * Return: Always 0 (Success)
  */
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-
 	if (argc >= 1)
 	{
-	
 		printf("%s\n", argv[0]);
 	}
 
