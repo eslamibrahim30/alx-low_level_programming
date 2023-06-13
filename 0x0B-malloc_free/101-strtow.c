cbf6b4d11a9a8bb6dcda491a22dccfdb947b668a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-13 11:21:49 -0700

    Store words in allocated array of strings

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,94 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * free_space - Frees a 2 dimensional grid previously created
+ * @grid: a pointer to the grid to be freed
+ * @height: the height of that grid
+ */
+void free_space(char **grid, unsigned int height)
+{
+	int i;
+
+	for (i = height - 1; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
+/**
+ * count_words - Counts the words in a string
+ * @str: the given string
+ *
+ * Return: the number of words in the given string
+ */
+unsigned int count_words(char *str)
+{
+	unsigned int nWords;
+	unsigned int i;
+
+	nWords = 0;
+	i = 0;
+	while (i < strlen(str))
+	{
+		if (str[i] != ' ')
+		{
+			while (str[i] != ' ')
+			{
+				i++;
+			}
+			nWords++;
+		}
+		i++;
+	}
+	return (nWords);
+}
+
+/**
+ * strtow - Splits a string into words
+ * @str: the given string
+ *
+ * Return: a pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	char **arr;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
+	unsigned int wordSize = 0;
+	unsigned int cWord = 0;
+	unsigned int nWords = 0;
+
+	if (str == NULL || strlen(str) == 0)
+		return (NULL);
+	nWords = count_words(str);
+	arr = (char **)malloc(nWords * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0' && cWord < nWords; i++)
+	{
+		if (str[i] != ' ')
+		{
+			j = i;
+			wordSize = 0;
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+			arr[cWord] = (char *)malloc((wordSize + 1) * sizeof(char));
+			if (arr[cWord] == NULL)
+			{
+				free_space(arr, cWord);
+				return (NULL);
+			}
+			for (k = 0; k < wordSize; k++)
+				arr[cWord][k] = str[j + k];
+			arr[cWord][wordSize] = '\0';
+			cWord++;
+		}
+	}
+	return (arr);
+}
