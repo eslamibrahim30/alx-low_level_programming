544d82f1c4ed353e6712fceefa8f11773bf7cc14
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-15 08:08:09 -0700

    Calloc using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,32 +11,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void **arr = NULL;
-	unsigned int i = 0;
-	unsigned int j = 0;
-
+	void *arr = NULL;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	arr = (void **)malloc(nmemb * sizeof(void *));
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
-	{
-		arr[i] = malloc(size);
-		if (arr[i] == NULL)
-		{
-			for (j = i - 1; ; j--)
-			{
-				free(arr[i]);
-			}
-			free(arr);
-			return (NULL);
-		}
-	}
-	return (*arr);
+	return (arr);
 }
