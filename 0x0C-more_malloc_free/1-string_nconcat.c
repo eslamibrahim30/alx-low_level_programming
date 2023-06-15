6d9145d55863e80952c35fe76880835acf8636a3
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-15 07:28:33 -0700

    Concatenate two strings using malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the number of bytes to be taken from the second string
+ *
+ * Return: pointer shall point to a newly allocated space in memory,
+ * which contains s1, followed by the first n bytes of s2,
+ * and null terminated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int size = 0;
+	char *concatStrings = NULL;
+
+	size = strlen(s1) + n + 1;
+	concatStrings = malloc(size);
+	if (concatStrings == NULL)
+	{
+		exit(98);
+	}
+	while (i < strlen(s1))
+	{
+		*(concatStrings +i) = s1[i];
+		i++;
+	}
+	j = 0;
+	size = strlen(s2) >= n ? n : strlen(s2);
+	while (j < size)
+	{
+		*(concatStrings + i) = s2[j];
+		i++;
+		j++;
+	}
+	*(concatStrings + i) = '\0';
+	return (concatStrings);
+}
