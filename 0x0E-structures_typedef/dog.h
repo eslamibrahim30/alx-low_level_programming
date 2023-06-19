557ee2f020890c9fe174f738957115bd4842e3e4
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-19 14:30:59 -0700

    First struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a new dog
+ * @name: the name of the new dog
+ * @age: the age of the new dog
+ * @owner: the name of the owner of the new dog
+ *
+ * Description: this data structure is used to store information
+ * about the new dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
