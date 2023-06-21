5bbadb97afebb45f1847861842148754eb8f65ea
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-21 16:18:18 -0700

    Print number of bytes of main code

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of arguments of the main function
+ * @argv: the actual arguments of the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int n;
+	int i;
+	char *p;
+	char s[20];
+	char c1;
+	char c2;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	p = &main;
+	for (i = 0; i < n; i++)
+	{
+		sprintf(s, "%x", *(p + i));
+		c1 = s[strlen(s) - 2];
+		c2 = s[strlen(s) - 1];
+		printf("%c", c1 == NULL ? '0' : c1);
+		printf("%c", c2 == NULL ? '0' : c2);
+		if (i == n - 1)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(" ");
+		}
+	}
+}
