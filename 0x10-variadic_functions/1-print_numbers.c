85b57747759698801c6facbfe6a8ea6f0d70a3c4
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-06-22 11:38:38 -0700

    Print numbers using a variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,6 +5,7 @@
 /**
  * print_numbers - Prints numbers, followed by a new line.
  * @separator: the string to be printed between numbers
+ * @n: the number of numbers to be printed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
