3074f978662f1a1bc438d18908b204a797f886c4
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-07-01 15:43:51 -0700

    First assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+global main
+extern main
+
+section .text
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, _hello
+	mov rdx, 17
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .data
+	_hello: db "Hello, Holberton", 10
