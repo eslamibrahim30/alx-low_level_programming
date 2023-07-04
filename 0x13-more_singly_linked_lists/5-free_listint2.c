a5f1e15207af678705f823779bd8ffff0ab4da79
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-07-04 15:11:07 -0700

    Free list using iterative function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: the address of the head of the list to which we add the new node.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *ptr_next;
+
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		ptr_next = ptr->next;
+		free(ptr);
+		ptr = ptr_next;
+	}
+	*head = NULL;
+}
