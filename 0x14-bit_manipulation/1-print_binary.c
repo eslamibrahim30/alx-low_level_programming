ec820d85857e3176d4ad831faf3ad6edacbe92e2
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-07-06 09:53:05 -0700

    Print number in binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,24 +7,28 @@
  */
 void print_binary(unsigned long int n)
 {
-	int size = 0;
-	unsigned long int max_2 = 1;
+	unsigned long int size = 0;
+	unsigned long int copy_n = n;
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	while ((max_2 << size) < n)
-		size++;
-	if ((max_2 << size) > n)
-		size--;
-	while (size >= 0)
+	while (copy_n)
 	{
-		if ((max_2 << size) & n)
+		copy_n >>= 1;
+		size++;
+	}
+	size--;
+	while (1)
+	{
+		if ((n >> size) & 1)
 			_putchar('1');
 		else
 			_putchar('0');
+		if (size == 0)
+			break;
 		size--;
 	}
 }
