cd2b9c12e397e7b6482896d0767d5faf9ed01e3a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-17 05:13:43 -0700

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,28 +25,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		cur_node = cur_node->next;
 		len++;
 	}
-	if (idx >= len)
+	if (idx > len)
 		return (NULL);
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 	cur_node = *h;
 	if (cur_node == NULL)
-	{
-		new_node->prev = NULL;
-		new_node->next = NULL;
 		*h = new_node;
-		return (new_node);
-	}
-	while (cur_idx != idx)
+	else if (idx == len)
 	{
-		cur_node = cur_node->next;
-		cur_idx++;
+		while (cur_node->next != NULL)
+			cur_node = cur_node->next;
+		cur_node->next = new_node;
 	}
-	cur_node->prev->next = new_node;
-	new_node->prev = cur_node->prev;
-	new_node->next = cur_node;
-	cur_node->prev = new_node;
+	else
+	{
+		while (cur_idx++ != idx)
+			cur_node = cur_node->next;
+		cur_node->prev->next = new_node;
+		new_node->prev = cur_node->prev;
+		new_node->next = cur_node;
+		cur_node->prev = new_node;
+	}
 	return (new_node);
 }
