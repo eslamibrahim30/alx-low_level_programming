ec15cd9b9e75684c74cdf6c8bf9025f3bb9e703a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-08-17 05:27:57 -0700

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,6 +3,24 @@
 #include <stdlib.h>
 
 /**
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list.
+ * @h: the head of the list.
+ *
+ * Return: the number of elements in the list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
+
+/**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
  * @h: a pointer to the head of the list.
  * @idx: the index of the list where the new node should be added.
@@ -17,15 +35,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int cur_idx = 0;
 	unsigned int len = 0;
 
-	if (h == NULL || (*h == NULL && idx != 0))
-		return (NULL);
-	cur_node = *h;
-	while (cur_node != NULL)
-	{
-		cur_node = cur_node->next;
-		len++;
-	}
-	if (idx > len)
+	len = dlistint_len(h == NULL ? NULL : *h);
+	if (h == NULL || (*h == NULL && idx != 0) || idx > len)
 		return (NULL);
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -36,6 +47,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	cur_node = *h;
 	if (cur_node == NULL)
 		*h = new_node;
+	else if (idx == 0)
+	{
+		new_node->next = cur_node;
+		cur_node->prev = new_node;
+		*h = new_node;
+	}
 	else if (idx == len)
 	{
 		while (cur_node->next != NULL)
