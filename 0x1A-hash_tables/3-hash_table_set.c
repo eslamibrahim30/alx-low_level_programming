bc1ebaa1a76dd3f156ed58c137f089f14e52b802
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-09-08 09:56:28 -0700

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,10 +24,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
+	{
+		free(new_node);
 		return (0);
+	}
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
 		return (0);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
