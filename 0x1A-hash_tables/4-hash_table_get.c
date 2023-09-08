54e6b332204a99d6b5fa4d9e9580ca19d797f1ec
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-09-08 13:03:11 -0700

    >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: the given hash table.
+ * @key: the key you are looking for.
+ *
+ * Return: The value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *ptr = NULL;
+
+	if (ht == NULL || ht->size == 0 || key == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[index];
+	while(ptr != NULL)
+	{
+		if (strcmp(key, ptr->key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
