9044cf34066d7aed95861302c335c4df95a4a7f3
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-09-08 08:19:15 -0700

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 /**
  * hash_table_set - Adds an element to the hash table.
@@ -15,14 +16,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	unsigned long int index = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
