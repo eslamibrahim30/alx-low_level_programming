a01f358b9547bcce11d09f3fa9d91031d094381b
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-12-13 20:15:54 +0200

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,17 +8,13 @@
  * @size: the number of elements in array.
  * @value: the value to search for.
  *
- * Description: Searches for a value in an array of integers using the
- * linear search algorithm.
  * Return: the first index where value is located.
- * If value is not present in array or
- * if array is NULL, the function must return -1
  */
 int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i = 0;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 	{
 		return (-1);
 	}
