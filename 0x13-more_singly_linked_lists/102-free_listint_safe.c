cf5371d0991735b54a2a1f28888d30018220b868
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-08-30 17:31:45 +0300

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -13,25 +13,25 @@ size_t free_listint_safe(listint_t **h)
 	size_t size = 0;
 	listint_t *ptr_a = NULL;
 	listint_t *ptr_b = NULL;
-	listint_t *ptr_loop_fast = NULL;
+	listint_t *ptr_fast = NULL;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 	ptr_a = *h;
 	ptr_b = ptr_a->next;
-	ptr_loop_fast = ptr_a->next;
+	ptr_fast = ptr_a->next;
 	while (ptr_a != NULL)
 	{
-		if (ptr_loop_fast->next != NULL && ptr_loop_fast->next->next == ptr_b)
-			ptr_loop_fast->next->next = NULL;
-		else
-			ptr_loop_fast = ptr_loop_fast->next->next;
+		if (ptr_fast->next != NULL && ptr_fast->next->next == ptr_b && ptr_b != NULL)
+			ptr_fast->next->next = NULL;
 		ptr_a->next = NULL;
 		free(ptr_a);
 		size++;
 		ptr_a = ptr_b;
 		if (ptr_b != NULL)
 			ptr_b = ptr_a->next;
+		if (ptr_fast->next != NULL)
+			ptr_fast = ptr_fast->next->next;
 	}
 	*h = NULL;
 	return (size);
