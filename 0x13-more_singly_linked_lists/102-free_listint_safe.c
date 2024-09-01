7aab6b342ac9b28b4e9158463631ab1f2c467da0
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-09-01 10:56:38 +0300

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -25,7 +25,8 @@ size_t free_listint_safe(listint_t **h)
 	{
 		if (ptr_fast_prev != NULL && ptr_fast == ptr_slow)
 			ptr_fast_prev->next = NULL;
-		ptr_fast_prev = ptr_fast->next;
+		if (ptr_fast != NULL)
+			ptr_fast_prev = ptr_fast->next;
 		if (ptr_fast_prev != NULL)
 			ptr_fast = ptr_fast_prev->next;
 		ptr_slow_next = ptr_slow->next;
