d55894536ba01f98d9b786ccf8061739889f3bdf
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-09-02 17:48:44 +0300

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 #include "lists.h"
 
+
+/**
+ * find_listint_loop - Finds a loop in a listint_t linked list.
+ * @head: the head of the linked list.
+ *
+ * Return: the address of the node where the loop starts,
+ * Or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr_slow = NULL;
+	listint_t *ptr_fast = NULL;
+	listint_t *ptr_fast_prev = NULL;
+
+	ptr_slow = head;
+	ptr_fast = head;
+	while (1)
+	{
+		if (ptr_slow != NULL || ptr_fast != NULL || ptr_fast->next != NULL)
+			return (NULL);
+		ptr_slow = ptr_slow->next;
+		ptr_fast_prev = ptr_fast->next;
+		ptr_fast = ptr_fast->next->next;
+		if (ptr_fast == ptr_slow)
+			break;
+	}
+	ptr_slow = head;
+	while (ptr_slow != ptr_fast)
+	{
+		ptr_slow = ptr_slow->next;
+		ptr_fast_prev = ptr_fast;
+		ptr_fast = ptr_fast->next;
+	}
+	return (ptr_fast_prev);
+}
+
+
 /**
  * free_listint_safe - Frees a listint_t list.
  * @h: a pointer to the head of the list.
@@ -11,33 +48,23 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t size = 0;
-	listint_t *ptr_slow = NULL;
-	listint_t *ptr_slow_next = NULL;
-	listint_t *ptr_fast = NULL;
-	listint_t *ptr_fast_prev = NULL;
+	listint_t *ptr = NULL;
+	listint_t *ptr_next = NULL;
+	listint_t *ptr_loop_start = NULL;
 
 	if (h == NULL || *h == NULL)
 		return (0);
-	ptr_slow = *h;
-	ptr_fast_prev = ptr_slow;
-	ptr_fast = ptr_slow;
-	while (ptr_slow != NULL)
+	ptr_loop_start = find_listint_loop(*h);
+	if (ptr_loop_start != NULL)
+		ptr_loop_start->next = NULL;
+	ptr = *h;
+	while (ptr != NULL)
 	{
-		if (ptr_fast != NULL)
-			ptr_fast_prev = ptr_fast->next;
-		if (ptr_fast_prev != NULL)
-			ptr_fast = ptr_fast_prev->next;
-		if (ptr_fast_prev != NULL && (ptr_fast == ptr_slow || ptr_fast == *h))
-		{
-			ptr_fast_prev->next = NULL;
-			ptr_fast = NULL;
-			ptr_fast_prev = NULL;
-		}
-		ptr_slow_next = ptr_slow->next;
-		ptr_slow->next = NULL;
-		free(ptr_slow);
+		ptr_next = ptr->next;
+		ptr->next = NULL;
+		free(ptr);
 		size++;
-		ptr_slow = ptr_slow_next;
+		ptr = ptr_next;
 	}
 	*h = NULL;
 	return (size);
